8be746f1e3fe0e28d4feaeb70ece9940e5c74a97
imduchuyyy <duchuy.124dk@gmail.com>
2025-01-15 13:27:28 +0700

    feat: add solc version and new apply script

--- script/Apply.s.sol
+++ script/Apply.s.sol
@@ -5,6 +5,7 @@ import "forge-std/Script.sol";
 import "forge-std/console.sol";
 
 import "../src/Stealth.sol";
+
 contract StealthScript is Script {
     address issuer = 0x8c0faeb5C6bEd2129b8674F262Fd45c4e9468bee;
 
@@ -12,11 +13,13 @@ contract StealthScript is Script {
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address deployerAddress = vm.addr(deployerPrivateKey);
 
         vm.startBroadcast(deployerPrivateKey);
-        Stealth stealthContract = new Stealth(deployerAddress);
-        console.log("Stealth contract address: ", address(stealthContract));
+        (bool success, ) = issuer.call{value: 10 ether}(
+            abi.encodeWithSignature("apply(address)", address(0x5715729dcfFc6717eb53D4E4446322368d4cF7F3))
+        );
+        require(success, "Failed to apply");
+        console.log("Applied to issuer");
         vm.stopBroadcast();
     }
 }
